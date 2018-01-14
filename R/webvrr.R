22bed0102f2b23065d0a6896efd5dcd6af4f25ed
Scott Jackson <scottmmjackson@gmail.com>
2018-01-14 10:40:46 -0500

    0.0.2 major update

--- R/webvrr.R
+++ R/webvrr.R
@@ -6,27 +6,133 @@
 #'
 #' @param width The width of the viewport prior to entering vr mode
 #' @param height The height of the viewport prior to entering vr mode
-#' @param sphere Either a url or a base64 encoded `src` to be drawn on the sphere
-#'
-#' @examples
-#' webvrr(sphere =
-#' 'https://upload.wikimedia.org/wikipedia/commons/8/83/Equirectangular_projection_SW.jpg'
-#' )
 #'
 #' @import htmlwidgets
 #'
 #' @export
 webvrr <- function(
   width = NULL,
-  height = NULL,
-  sphere = NULL
+  height = NULL
 ) {
   htmlwidgets::createWidget(
     name = "webvrr",
     x = list(
-      sphere = sphere
+      calls = list()
     ),
     width = width,
     height = height
   )
+}
+
+#' Add a background to a WebVR widget.
+#' For drawing a skybox sphere, use \code{\link{addInnerTexturedSphere}}
+#'
+#' @param vr A webvr widget. See \code{\link{webvrr}}
+#' @param backgroundType one of "color", "texture", or "cube"
+#' @param backgroundColor Hex or numeric color representation. Required if backgroundType is "color".
+#'        The function call tolerates string and hexmode representations.
+#' @param texture a string representing the texture in a base64 data URI or a URL. Required if
+#'        backgroundType is "texture".
+#' @param shouldRepeat should the texture repeat?
+#' @param repeatX repeat density over X portion of mesh
+#' @param repeatY repeat density over Y portion of mesh
+#' @param cube a list of 6 strings representing base64 data URIs or URLs corresponding to a cube map.
+#'        required if backgroundType is "cube".
+#' @export
+addBackground <- function(
+  vr,
+  backgroundType = c("color", "texture", "cube"),
+  backgroundColor = NULL,
+  texture = NULL,
+  shouldRepeat = FALSE,
+  repeatX = NULL,
+  repeatY = NULL,
+  cube = list()
+) {
+  if (backgroundType == "color") {
+    if(is.character(backgroundColor)) backgroundColor <- as.hexmode(backgroundColor)
+    stopifnot(is.numeric(backgroundColor))
+    vr$x$background <- list(
+      type = backgroundType,
+      color = backgroundColor
+    )
+  } else if (backgroundType == "texture") {
+    stopifnot(is.character(texture))
+    if (shouldRepeat) {
+      stopifnot(is.numeric(repeatX) && is.numeric(repeatY))
+      vr$x$background <- list(
+        type = backgroundType,
+        texture = texture,
+        `repeat` = list(
+          repeatX = repeatX,
+          repeatY = repeatY
+        )
+      )
+    } else {
+      vr$x$background <- list(
+        type = backgroundType,
+        texture = texture,
+        `repeat` = FALSE
+      )
+    }
+  } else if (backgroundType == "cube") {
+      stopifnot(length(cube) == 6)
+      vr$x$background <- list(
+        type = backgroundType,
+        cube = cube
+      )
+  } else {
+    stop("Unknown backgroundType")
+  }
+  vr
+}
+
+#' Add an arbitrary object to the scene
+#' Internal.
+#'
+#' @param vr a webvrr widget
+#' @param call a specially crafted call object
+addToScene <- function(
+  vr,
+  call = list()
+) {
+  vr$x$calls <- lapply(1:(length(vr$x$calls)+1), function(y) {
+    if(y > length(vr$x$calls)) call
+    else vr$x$calls[[y]]
+  })
+  vr
+}
+
+#' Puts the viewer in a big sphere with a texture painted on the inside.
+#' See https://threejs.org/docs/#api/geometries/SphereGeometry
+#'
+#' @param vr a webvrr widget. See \code{\link{webvrr}}
+#' @param texture a string representing a texture in base64 data URI format or a URL.
+#'                if not provided, a wireframe will be used
+#' @param repeat If false, do not repeat. Otherwise, should be a list in the following
+#'               format: \code{list( repeatX = 32, repeatY = 32)}. Replace 32 with your
+#'               preferred repeats.
+#' @param radius sphere radius.
+#' @param widthSegments how many width segments to draw
+#' @param heightSegments how many height segments to draw
+#' @export
+addInnerTexturedSphere <- function(
+  vr,
+  texture = NULL,
+  `repeat`= list(
+    repeatX = 32,
+    repeatY = 32
+  ),
+  radius = 5,
+  widthSegments = 32,
+  heightSegments = 32
+) {
+  if(`repeat` != FALSE) stopifnot(is.list(`repeat`))
+  addToScene(vr, list(
+    type = "innerTexturedSphere",
+    `repeat` = `repeat`,
+    radius = radius,
+    widthSegments = widthSegments,
+    heightSegments = heightSegments
+  ))
 }
--- javascript/src/webvrr.js
+++ javascript/src/webvrr.js
@@ -1,4 +1,6 @@
+/* eslint-disable no-param-reassign,no-undef */
 // @flow
+
 import document from './global/document';
 import window from './global/window';
 import navigator from './global/navigator';
@@ -8,77 +10,206 @@ import THREE from './global/three';
 import WebVRPolyfill from './global/webvrpolyfill';
 import webvrui from './global/webvrui';
 import Math from './global/math';
-
+import target from './target';
 import wireframe from './wireframe';
 
 // backwards compatibility with old versions of webvr-polyfill
 // lots of API breaking changes across versions...
 const polyfill = (typeof WebVRPolyfill === 'function') ?
   new WebVRPolyfill({}) :
-  { enable() {} };
+  {
+    enable() {
+    },
+  };
 polyfill.enable();
+
+type Scene = {
+  add: ({}) => void;
+  background: {},
+}
+
+type Camera = {
+  aspect: number,
+  updateProjectionMatrix: () => void,
+}
+
+type Effect = {
+  render: (Scene, Camera) => void,
+  setSize: (number, number) => void,
+}
+
+type AnimateFactoryOptions = {
+  vrDisplay: {
+    requestAnimationFrame: ((number) => void) => void,
+  },
+  lastRenderTime: number,
+  vrButton: {
+    isPresenting: () => boolean,
+  },
+  effect: Effect,
+  controls: {
+    update: () => void,
+  },
+  scene: Scene,
+  camera: Camera,
+}
+
+type OnResizeFactoryOptions = {
+  effect: Effect,
+  camera: Camera,
+  el: HTMLElement,
+}
+
+type Background = {
+  type: "color",
+  color: number,
+} | {
+  type: "texture",
+  texture: string,
+  repeat: false | {
+    repeatX: number,
+    repeatY: number,
+  }
+} | {
+  type: "cube",
+  cube: [
+    string,
+    string,
+    string,
+    string,
+    string,
+    string,
+    ]
+};
+
+type Call = {
+  type: "innerTexturedSphere",
+  texture?: string,
+  repeat: false | {
+    repeatX: number,
+    repeatY: number,
+  },
+  radius: number,
+  widthSegments: number,
+  heightSegments: number,
+};
+
+type X = {
+  background?: Background,
+  calls?: Array<Call>,
+}
+
+function onResizeFactory({ effect, camera, el }: OnResizeFactoryOptions) {
+  return function onResize(e) {
+    if (e && e.vrDisplay && e.vrDisplay.isPresenting) {
+      effect.setSize(window.innerWidth, window.innerHeight);
+      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.updateProjectionMatrix();
+    } else {
+      effect.setSize(el.clientWidth, el.clientHeight);
+      camera.aspect = el.clientWidth / el.clientHeight;
+      camera.updateProjectionMatrix();
+    }
+  };
+}
+
+function animateFactory(options: AnimateFactoryOptions) {
+  const {
+    vrDisplay,
+    vrButton,
+    effect,
+    controls,
+    scene,
+    camera,
+    lastRenderTime,
+  } = options;
+  const callbacks = [];
+  return (timestamp: number): void => {
+    const delta = Math.min(timestamp - lastRenderTime, 500);
+    if (vrButton.isPresenting()) {
+      controls.update();
+    }
+    callbacks.map(cb => cb(delta));
+    effect.render(scene, camera);
+    const animate = animateFactory({
+      ...options,
+      lastRenderTime: timestamp,
+    });
+    vrDisplay.requestAnimationFrame(animate);
+  };
+}
+
+function setBackground(scene: Scene, background?: Background) {
+  if (!background) { return; }
+  switch (background.type) {
+    case 'color': {
+      scene.background = new THREE.Color(background.color);
+      break;
+    }
+    case 'texture': {
+      scene.background = new THREE.TextureLoader().load(background.texture);
+      if (background.repeat) {
+        scene.background.wrapS = THREE.RepeatWrapping;
+        scene.background.wrapT = THREE.RepeatWrapping;
+        scene.background.repeat.set(background.repeat.repeatX, background.repeat.repeatY);
+      }
+      break;
+    }
+    case 'cube': {
+      scene.background = new THREE.CubeTextureLoader().load(background.cube);
+      break;
+    }
+    default:
+      break;
+  }
+}
+
+function doCall(c: Call, scene: Scene) {
+  switch (c.type) {
+    case 'innerTexturedSphere': {
+      const {
+        texture, repeat, radius, widthSegments, heightSegments,
+      } = c;
+      let loadTex;
+      if (texture === 'wireframe' || texture !== null) {
+        loadTex = wireframe;
+      } else if (texture === 'target') {
+        loadTex = target;
+      } else {
+        loadTex = texture;
+      }
+      const geometry = new THREE.SphereGeometry(radius, widthSegments, heightSegments);
+      const material = new THREE.MeshBasicMaterial({
+        map: new THREE.TextureLoader().load(loadTex),
+        color: 0x01BE00,
+        side: THREE.BackSide,
+      });
+      if (repeat) {
+        material.map.wrapS = THREE.RepeatWrapping;
+        material.map.wrapT = THREE.RepeatWrapping;
+        material.map.repeat.set(repeat.repeatX, repeat.repeatY);
+      }
+      // Align the skybox to the floor (which is at y=0).
+      const sphere = new THREE.Mesh(geometry, material);
+      sphere.position.y = radius / 2;
+      scene.add(sphere);
+      break;
+    }
+    default:
+      break;
+  }
+}
 
 HTMLWidgets.widget({
   name: 'webvrr',
   type: 'output',
   factory(el, width, height) {
     let vrButton;
-    let geometry;
-    let material;
-    let skybox;
-    let texture;
-
-    const renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setPixelRatio(window.devicePixelRatio);
-    el.appendChild(renderer.domElement);
-
-    const scene = new THREE.Scene();
-
-    const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 10000);
-
-    const controls = new THREE.VRControls(camera);
-    controls.standing = true;
-    camera.position.y = controls.userHeight;
-
-    const effect = new THREE.VREffect(renderer);
-    effect.setSize(width, height);
-
-    let lastRenderTime = 0;
-
-    function animateFactory(vrDisplay) {
-      const callbacks = [];
-      return (timestamp) => {
-        const delta = Math.min(timestamp - lastRenderTime, 500);
-        lastRenderTime = timestamp;
-        if (vrButton.isPresenting()) {
-          controls.update();
-        }
-        callbacks.map(cb => cb(delta));
-        effect.render(scene, camera);
-        const animate = animateFactory(vrDisplay);
-        vrDisplay.requestAnimationFrame(animate);
-      };
-    }
-
-    navigator.getVRDisplays().then((displays) => {
-      if (displays.length > 0) {
-        const vrDisplay = displays[0];
-        const animate = animateFactory(vrDisplay);
-        vrDisplay.requestAnimationFrame(animate);
-      }
-    });
-
-    const onResize = (e) => {
-      if (e && e.vrDisplay && e.vrDisplay.isPresenting) {
-        effect.setSize(window.innerWidth, window.innerHeight);
-        camera.aspect = window.innerWidth / window.innerHeight;
-        camera.updateProjectionMatrix();
-      } else {
-        effect.setSize(el.innerWidth, el.innerHeight);
-        camera.aspect = el.innerWidth / el.innerHeight;
-        camera.updateProjectionMatrix();
-      }
-    };
+    let renderer;
+    let scene;
+    let camera;
+    let controls;
+    let effect;
 
     const uiOptions = {
       color: 'black',
@@ -86,59 +217,81 @@ HTMLWidgets.widget({
       corners: 'square',
     };
 
-    vrButton = new webvrui.EnterVRButton(renderer.domElement, uiOptions);
-
-    const uiContainer = document.createElement('div');
-    uiContainer.className = 'vr-ui-container';
-
-    const vrButtonContainer = document.createElement('div');
-    vrButtonContainer.id = 'vr-button';
-    vrButtonContainer.className = 'vr-button-container';
-    vrButtonContainer.appendChild(vrButton.domElement);
-    uiContainer.appendChild(vrButtonContainer);
-
-    const vrForceContainer = document.createElement('div');
-    vrForceContainer.id = 'vr-force';
-    vrForceContainer.className = 'vr-force-container';
-    const vrForceLink = document.createElement('a');
-    vrForceLink.href = '#';
-    vrForceLink.innerText = 'Click Here To Try With A Browser';
-    vrForceLink.addEventListener('click', () => {
-      vrButton.requestEnterFullscreen().then(() => {
-        onResize({ vrDisplay: { isPresenting: true }});
-      });
-    });
-    vrForceContainer.appendChild(vrForceLink);
-    uiContainer.appendChild(vrForceContainer);
-
-    el.appendChild(uiContainer);
-
-    window.addEventListener('vrdisplaypresentchange', onResize, true);
-
     return {
+      getScene() {
+        return scene;
+      },
       resize(newWidth, newHeight) {
         effect.setSize(newWidth, newHeight);
         camera.aspect = newWidth / newHeight;
         camera.updateProjectionMatrix();
       },
-      renderValue(x) {
-        texture = new THREE.TextureLoader().load(wireframe);
-        texture.wrapS = THREE.RepeatWrapping;
-        texture.wrapT = THREE.RepeatWrapping;
-        texture.repeat.set(20, 20);
-        geometry = new THREE.SphereGeometry(30, 32, 32);
-        geometry.scale(-1, 1, 1);
-        material = new THREE.MeshBasicMaterial({
-          map: texture,
+      renderValue({
+        // eslint-disable-next-line no-unused-vars
+        background, calls,
+      }: X) {
+        renderer = new THREE.WebGLRenderer({ antialias: true });
+        renderer.setPixelRatio(window.devicePixelRatio);
+        el.appendChild(renderer.domElement);
+
+        scene = new THREE.Scene();
+        setBackground(scene, background);
+
+        camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 10000);
+
+        controls = new THREE.VRControls(camera);
+        controls.standing = true;
+        camera.position.y = controls.userHeight;
+
+        effect = new THREE.VREffect(renderer);
+        effect.setSize(width, height);
+
+        const onResize = onResizeFactory({ effect, camera, el });
+        window.addEventListener('vrdisplaypresentchange', onResize, true);
+
+        vrButton = new webvrui.EnterVRButton(renderer.domElement, uiOptions);
+        const uiContainer = document.createElement('div');
+        uiContainer.className = 'vr-ui-container';
+
+        const vrButtonContainer = document.createElement('div');
+        vrButtonContainer.id = 'vr-button';
+        vrButtonContainer.className = 'vr-button-container';
+        vrButtonContainer.appendChild(vrButton.domElement);
+        uiContainer.appendChild(vrButtonContainer);
+
+        const vrForceContainer = document.createElement('div');
+        vrForceContainer.id = 'vr-force';
+        vrForceContainer.className = 'vr-force-container';
+
+        const vrForceLink = document.createElement('a');
+        vrForceLink.href = '#';
+        vrForceLink.innerText = 'Click Here To Try With A Browser';
+        vrForceLink.addEventListener('click', () => {
+          vrButton.requestEnterFullscreen().then(() => {
+            onResize({ vrDisplay: { isPresenting: true } });
+          });
         });
-        skybox = new THREE.Mesh(geometry, material);
-        // skybox.position.y = 30 / 2;
-        scene.add(skybox);
-        if (x) {
-          if (x.sphere) {
-            material.map = new THREE.TextureLoader().load(x.sphere);
+        vrForceContainer.appendChild(vrForceLink);
+        uiContainer.appendChild(vrForceContainer);
+        el.appendChild(uiContainer);
+
+        (calls || []).forEach(c => doCall(c, scene));
+
+        navigator.getVRDisplays().then((displays) => {
+          if (displays.length > 0) {
+            const vrDisplay = displays[0];
+            const animate = animateFactory({
+              vrDisplay,
+              vrButton,
+              effect,
+              scene,
+              camera,
+              controls,
+              lastRenderTime: 0,
+            });
+            vrDisplay.requestAnimationFrame(animate);
           }
-        }
+        });
       },
     };
   },
